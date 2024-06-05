0b76b824abd92ccc42400ef7c4e960402a30c79e
ktjw16 <ktjw16@gmail.com>
2024-06-04 22:52:49 -0700

    Update formatting and photos

--- js/script.js
+++ js/script.js
@@ -1,22 +1,3 @@
-if (document.getElementById('my-work-link')) {
-  document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('about-section').scrollIntoView({behavior: "smooth"})
-  })
-}
-
-function openNav() {
-  document.getElementById("sidenav").style.width = "100%";
-  document.getElementById("footer").style.width = "100%";
-  document.getElementById("sidenav").style.position = "relative";
-  document.getElementById("footer").style.position = "relative";
-  // document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("sidenav").style.width = "0";
-  document.getElementById("footer").style.width = "0";
-} 
-
 function toggleNavOverlay() {
   var x = document.getElementById("sidenav");
   var y = document.getElementById("footer");
